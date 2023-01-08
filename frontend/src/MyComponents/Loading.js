116aecc108d9ad6a6bf7464d9ebcbe0daeb2870e
Shubham Kunal <shubhamkunal19@gmail.com>
2023-01-09 02:55:28 +0530

    Create Loading.js

---
+++ frontend/src/MyComponents/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Loading = () => {
+  return (
+    <div>
+      <div>Free server is slow. So, please wait!</div>
+      <div>
+        <img src="/loading.gif" alt="Loading animation" />
+      </div>
+    </div>
+  )
+}
